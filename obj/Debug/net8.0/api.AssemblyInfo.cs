15fc64812877c1cb0d853c7535cf8539b0652077
Lawrence John Balanza <ljdb1999@gmail.com>
2025-03-23 20:02:39 -0700

    3.23.2025 async await

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4403d810dabf822841b772eb39d3959459295df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0dd1b90ba2fe7094745c76975131a6cd535723")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
