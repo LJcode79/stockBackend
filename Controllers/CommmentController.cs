85c9a60b8d71830269a52e93e86877ae537dcb64
Lawrence John Balanza <ljdb1999@gmail.com>
2025-03-24 16:26:42 -0700

    3.24.2025 Started Comment repository and controller

---
+++ Controllers/CommmentController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommmentController : ControllerBase
+    {
+        
+    }
+}
